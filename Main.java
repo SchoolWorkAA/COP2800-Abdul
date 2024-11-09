6e8e0d08073be6088f6ebd28bba4a9d11e5c84e4
SchoolWorkAA <aalnaggar2004@gmail.com>
2024-11-09 15:06:10 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,22 +1,41 @@
 import java.util.Scanner;
 
 public class Main {
+
+    //This is the code that figures out the array for the integers.
+    public static int average(int[] array) {
+        int sum = 0;
+        for (int num : array) {
+            sum += num;
+        }
+        return sum / array.length;
+    }
+
+    //This code will calculate the average amount of the double array.
+    public static double average(double[] array) {
+        double sum = 0.0;
+        for (double num : array) {
+            sum += num;
+        }
+        return sum / array.length;
+    }
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+ 
+        //This code will make a print for the user to enter 10 double values.
+        double[] values = new double[10];
+        System.out.println("Enter 10 double values:");
 
-        // This code makes a print that allows the users to input the subtotal first.
-        
-        System.out.print("Enter the subtotal: ");
-        double subtotal = input.nextDouble();
+        for (int i = 0; i < 10; i++) {
+            values[i] = scanner.nextDouble();
+        }
 
-        // This code makes a print that allows the users to input the gratuity rate second.
-        // It also contains the equation to find it as well.
-        
-        System.out.print("Enter the gratuity rate: ");
-        double gratuity = subtotal * input.nextDouble() / 100;
+        double avg = average(values);
 
-        //This code makes a print which shows what the gratuity is as well as the total.
-        
-        System.out.printf("The gratuity is $%.2f and total is $%.2f\n", gratuity, subtotal + gratuity);
+        //This code shows the averge of the values to the user in a print.
+        System.out.println("The average of the entered values is: " + avg);
+
+        scanner.close();
     }
 }
