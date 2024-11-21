bdc3afd03c9bfc10feec3be2dc91d254445ea8a9
SchoolWorkAA <aalnaggar2004@gmail.com>
2024-11-20 22:24:40 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        //This displays the messages, given by the assignment, to the user.
         System.out.println("Welcome to Java");
-        System.out.println("Welcome to Computer Science");
-        System.out.println("Programming is fun");
+            System.out.println("Welcome to Java");
+            System.out.println("Welcome to Java");
+            System.out.println("Welcome to Java");
+            System.out.println("Welcome to Java");
     }
 }
