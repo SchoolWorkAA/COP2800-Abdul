1982da026f5bf3ad15e6b48495981bbde9c613d8
SchoolWorkAA <aalnaggar2004@gmail.com>
2024-11-21 19:24:17 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,27 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        //This print asks for the month and year that the user wants to input, and lets them             input it.
-        System.out.print("Enter your given month from 1 to 12: ");
-        int month = input.nextInt();
-        System.out.print("Enter your given year: ");
-        int year = input.nextInt();
+        //Makes a print that asks the user to input the length of the hexagon.
+        System.out.print("Enter the side length of the hexagon: ");
+        double side = input.nextDouble();
 
-        int days;
+        //Using the formula given in a picture in the assignment, this is the equation to find
+        //the area.
+        double area = (6 * Math.pow(side, 2)) / (4 * Math.tan(Math.PI / 6));
 
-        //This code checks if there is a leap year.
-        boolean isLeapYear = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
-
-        // This code is the "equation" that calculates the number of days in the given months             inputted by the user. It also takes itno account the year.
-        if (month == 2) {
-            days = isLeapYear ? 29 : 28;
-        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
-            days = 30;
-        } else {
-            days = 31;
-        }
-
-        //This print takes the result of the equation and prints it out for the user to see.
-        System.out.println("Number of days: " + days);
+        //This displays the area of the hexagon, given from the equation, to the user.
+        System.out.printf("The area of the hexagon is: %.2f%n", area);
     }
 }
+
